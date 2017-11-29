c210dbe47edc5fe273d372b36804bb64ebd4f686
Amanda <jamesamanda91@gmail.com>
2017-11-29 16:16:26 +0800

    committed by Amanda

--- springbootapp/src/main/java/com/example/springboot/springbootapp/SpringbootappApplication.java
+++ springbootapp/src/main/java/com/example/springboot/springbootapp/SpringbootappApplication.java
@@ -8,5 +8,7 @@ public class SpringbootappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootappApplication.class, args);
+
+		System.out.print("Testing by Amanda!");
 	}
 }
